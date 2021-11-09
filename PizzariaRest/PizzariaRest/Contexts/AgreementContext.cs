76ba1f12936d6cd06c1dbd5a172a02551b568b5c
Marcelo Duarte <marcelo.duarte@wellcare.com.br>
2021-11-09 18:59:16 -0300

    Criação de Agreement e configuração de Persistência usando EF Core InMemory. Exercícios: 1 - Terminar a Implementação da AgreementRepository. 2 - Retirar a Implementação da Coachee de Mock e passar para usar o EF Core da mesma maneira que Agreement.

---
+++ PizzariaRest/PizzariaRest/Contexts/AgreementContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PizzariaRest.Models;
+
+namespace PizzariaRest.Contexts
+{
+    public class AgreementContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("DbAgreement");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Agreement> Agreements { get; set; }
+    }
+}
---
+++ PizzariaRest/PizzariaRest/Repositories/AgreementRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using PizzariaRest.Contexts;
+using PizzariaRest.Interfaces;
+using PizzariaRest.Models;
+
+namespace PizzariaRest.Repositories
+{
+    public class AgreementRepository : IAgreementRepository
+    {
+        public AgreementRepository()
+        {
+        }
+
+        public IQueryable<Agreement> AgreementQueryable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Agreement Create(Agreement agreement)
+        {
+            using (AgreementContext dbContext = new AgreementContext())
+            {
+                dbContext.Add(agreement);
+                dbContext.SaveChanges();
+            }
+            return agreement;
+
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Agreement Read(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(string id, Agreement agreement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
